1b601881212e47db55edc5fa0771e625309183b0
Daniela Ciciliano <dann@Danielas-MacBook-Air.local>
2024-04-11 15:20:09 -0600

    Request to apliClient,Viewmodel & View Created,

---
+++ DuckDuckGoSearch/Views/SearchBar.swift
@@ -0,0 +1,33 @@
+//
+//  SearchBarV.swift
+//  DuckDuckGoSearch
+//
+//  Created by Daniela Ciciliano on 10/04/24.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @State var viewModel = SearchViewModel()
+    @Binding var text: String
+    
+    var body: some View {
+        HStack {
+            TextField("Search...", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            
+            Button(action: {
+                // Acción al presionar el botón de búsqueda
+                viewModel.search(query: text)
+            }) {
+                Text("Search")
+            }
+                .padding(.trailing)
+        }
+    }
+}
+
+/*#Preview {
+    SearchBar()
+}*/
